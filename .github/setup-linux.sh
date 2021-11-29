1b62336f2b1c7c9c04bcb53362e2f24e08514693
Doug Engert <deengert@gmail.com>
2021-11-29 12:57:04 -0600

    XXXXXXXX

--- .github/setup-linux.sh
+++ .github/setup-linux.sh
@@ -28,13 +28,12 @@ elif [ "$1" == "mingw" -o "$1" == "mingw32" ]; then
 #	https://wine.htmlvalidator.com/install-wine-on-ubuntu-20.04.html
 	dpkg --print-architecture
 	dpkg --print-foreign-architectures
-	DEPS="$DEPS xvfb wget"
+	DEPS="$DEPS wine wine xvfb wget"
 	sudo dpkg --add-architecture i386
 	if [ "$1" == "mingw" ]; then
-		DEPS="$DEPS binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 mingw-w64"
+		DEPS="$DEPS binutil win32 wine mingw-w64-x86-64 gcc-mingw-w64-x86-64 mingw-w64"
 	elif [ "$1" == "mingw32" ]; then
-
-		DEPS="$DEPS binutils-mingw-w64-i686 gcc-mingw-w64-i686"
+		DEPS="$DEPS wine32 binutils-mingw-w64-i686 gcc-mingw-w64-i686"
 	fi
 fi
 
@@ -47,13 +46,13 @@ sudo apt-get install --fix-broken --fix-missing -o Debug::pkgProblemResolver=yes
 
 if [ "$1" == "mingw" -o "$1" == "mingw32" ]; then
 
-	wget -nc https://dl.winehq.org/wine-builds/winehq.key
-	sudo -H gpg -o /etc/apt/trusted.gpg.d/winehq.key.gpg --dearmor winehq.key
-	sudo apt-key list
+#	wget -nc https://dl.winehq.org/wine-builds/winehq.key
+#	sudo -H gpg -o /etc/apt/trusted.gpg.d/winehq.key.gpg --dearmor winehq.key
+#	sudo apt-key list
 
-	sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-	sudo apt update
-	sudo apt install --install-recommends wine-stable
+#	sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+#	sudo apt update
+#	sudo apt install --install-recommends wine-stable
 
 	if [ ! -f "$(winepath 'C:/Program Files/Inno Setup 5/ISCC.exe')" ]; then
 		/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
