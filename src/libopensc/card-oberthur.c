98aaa1bff7a257fc9bb7119f619074c362c88f51
Simo Sorce <simo@redhat.com>
2023-02-15 16:45:15 -0500

    Convert card oberthur to use openssl libctx
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/libopensc/card-oberthur.c
+++ src/libopensc/card-oberthur.c
@@ -1336,6 +1336,7 @@ auth_update_component(struct sc_card *card, struct auth_update_component_info *a
 		const unsigned char in[8] = {0,0,0,0,0,0,0,0};
 		unsigned char out[8];
 		EVP_CIPHER_CTX  * ctx = NULL;
+                EVP_CIPHER *alg = NULL;
 
 		if (args->len!=8 && args->len!=24)
 			LOG_FUNC_RETURN(card->ctx, SC_ERROR_INVALID_ARGUMENTS);
@@ -1345,12 +1346,16 @@ auth_update_component(struct sc_card *card, struct auth_update_component_info *a
 		    LOG_FUNC_RETURN(card->ctx, SC_ERROR_OUT_OF_MEMORY);
 
 		p2 = 0;
-		if (args->len == 24)
-			EVP_EncryptInit_ex(ctx, EVP_des_ede(), NULL, args->data, NULL);
-		else
-			EVP_EncryptInit_ex(ctx, EVP_des_ecb(), NULL, args->data, NULL);
+		if (args->len == 24) {
+                        alg = sc_evp_cipher(card->ctx, "DES-EDE");
+			EVP_EncryptInit_ex(ctx, alg, NULL, args->data, NULL);
+		} else {
+                        alg = sc_evp_cipher(card->ctx, "DES-ECB");
+			EVP_EncryptInit_ex(ctx, alg, NULL, args->data, NULL);
+                }
 		rv = EVP_EncryptUpdate(ctx, out, &outl, in, 8);
 		EVP_CIPHER_CTX_free(ctx);
+                sc_evp_cipher_free(alg);
 		if (rv == 0) {
 			sc_log(card->ctx, "OpenSSL encryption error.");
 			LOG_FUNC_RETURN(card->ctx, SC_ERROR_INTERNAL);
