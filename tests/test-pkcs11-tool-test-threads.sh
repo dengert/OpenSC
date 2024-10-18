cd4504ff435ece7f548556e8cf1ea782f468314b
Doug Engert <deengert@gmail.com>
2024-10-18 13:53:10 -0500

    test-pkcs11-tool-test-threads.sh - needs a card to test some of the functions
     On branch X25519-improvements-2
     Changes to be committed:
    	modified:   test-pkcs11-tool-test-threads.sh

--- tests/test-pkcs11-tool-test-threads.sh
+++ tests/test-pkcs11-tool-test-threads.sh
@@ -3,8 +3,22 @@ SOURCE_PATH=${SOURCE_PATH:-..}
 
 source $SOURCE_PATH/tests/common.sh
 
+echo "======================================================="
+echo "Setup SoftHSM"
+echo "======================================================="
+if [[ ! -f $P11LIB ]]; then
+    echo "WARNING: The SoftHSM is not installed. Can not run this test"
+    exit 77;
+fi
+
+# The Ubuntu has old softhsm version not supporting this feature
+grep "Ubuntu 18.04" /etc/issue && echo "WARNING: Not supported on Ubuntu 18.04" && exit 77
+
+card_setup
+assert $? "Failed to set up card"
 # Test our PKCS #11 module here
-P11LIB="../src/pkcs11/.libs/opensc-pkcs11.so"
+# but we need a card to test some of the  tests i.e. slots
+# P11LIB="../src/pkcs11/.libs/opensc-pkcs11.so"
 
 echo "======================================================="
 echo "Test pkcs11 threads IN "
