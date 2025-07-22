6eae75d206f339266ec702b5dbbdaefb005c8270
Doug Engert <deengert@gmail.com>
2025-07-22 13:27:34 -0500

    pkcs15-lib.c -  oseid uses myeid driver and needs to bind
    if a oseid reader is found the short_name is changed from myeid to oseid
    which fails to bind in pkcs15init/pkcs15-lib.c
    Add one line to get bind to work in .github/test-oseid.sh

--- src/pkcs15init/pkcs15-lib.c
+++ src/pkcs15init/pkcs15-lib.c
@@ -155,6 +155,7 @@ static struct profile_operations {
 	{ "rutoken_ecp", (void *) sc_pkcs15init_get_rtecp_ops },
 	{ "rutoken_lite", (void *) sc_pkcs15init_get_rtecp_ops },
 	{ "myeid", (void *) sc_pkcs15init_get_myeid_ops },
+	{ "oseid", (void *) sc_pkcs15init_get_myeid_ops },
 	{ "sc-hsm", (void *) sc_pkcs15init_get_sc_hsm_ops },
 	{ "isoApplet", (void *) sc_pkcs15init_get_isoApplet_ops },
 	{ "gids", (void *) sc_pkcs15init_get_gids_ops },
